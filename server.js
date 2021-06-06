6bd3c75cd5e0a0460cf55d08f33c2a7219f1eb26
Dominick Tolomeo <tolomeodom@gmail.com>
2021-06-06 16:35:07 -0700

    server set up w/ handlebars, HTML converted to handlebars

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 var path = require('path')
 var express = require('express')
+var exphbs = require('express-handlebars')
 
-// var exphbs = require('express-handlebars')
+var app = express()
+var port = process.env.PORT || 3000
 
-var app = express();
-var port = process.env.PORT || 3000;
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+app.get('/', function(req, res) {
+    res.status(200).render('homePage')
+})
+
+app.get('/gallery', function(req, res) {
+    res.status(200).render('galleryPAge')
+})
+
+app.get('/about', function(req, res) {
+    res.status(200).render('aboutPage')
+})
 
 app.use(express.static('public'))
+
+app.get('*', function(req, res) {
+    res.status(404).render('404Page')
+})
 
 app.listen(port, function () {
     console.log("== Server is listening on port", port)
