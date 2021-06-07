596c0570d767217e807303e2632fc99b2e4d0d2d
CarterM00 <macnabc@oregonstate.edu>
2021-06-06 17:33:57 -0700

    temporary image

--- public/test.js
+++ public/test.js
@@ -1,7 +1,30 @@
-function append(link){
+var x,y;
+
+x = Math.floor(Math.random() * 900 ) + 1; 
+y = Math.floor(Math.random() * 700) + 1; 
+
+console.log("x==", x)
+console.log("y==", y)
+
+mergeImages([
+    {src: './resources/doing-business-hongkong.jpg'}, 
+    {src: './resources/robHess.png', x: x, y: y},
+])
+
+.then(b64 => document.getElementsByClassName('puzzle-photo')[0].src = b64);
+
+
+var e2 = document.getElementsByClassName('puzzle-photo')[0]
+
+e2.onclick = e => {
+
     
-    var img = document.createElement("img")
-    twitlink.href = "#"
-    twitlink.appendChild(linkz)
-    
+    console.log(e.offsetX)
+    console.log(e.offsetY)
+
+    if(e.offsetX >= x - 55 && e.offsetX <= x + 55){
+        if(e.offsetY >= y - 65 && e.offsetY <= y + 65) {
+            console.log("success!")
+        } 
+    }
 }
