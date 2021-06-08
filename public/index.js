42898221ddc0be3ae667d70b05da352f3b76adc3
Dominick Tolomeo <tolomeodom@gmail.com>
2021-06-07 23:15:08 -0700

    Post request for liking photos and adding them to Gallery

--- public/index.js
+++ public/index.js
@@ -25,6 +25,7 @@ findRoute()
 
 
 var x,y;
+var randx,randy;
 
 var width = document.querySelector('.puzzle-photo').width - 50
 var height = document.querySelector('.puzzle-photo').height - 50
@@ -43,42 +44,81 @@ next.addEventListener('click', function(){
 })
 
 
-e2.onclick = e => {
-
-    console.log(e.offsetX)
-    console.log(e.offsetY)
-
-    if(e.offsetX >= x - 80 && e.offsetX <= x + 55){
-        if(e.offsetY >= y - 65 && e.offsetY <= y + 65) {
-            console.log("success!")
-        } 
-    }
-}
 
 window.onload  = function(){
     makeimg(c)
 }
 
-function selectimg(){
+function selectimg() {
     location.reload()
 }
 
 
 function makeimg(imgpth){
-
+    
     randx = Math.floor(Math.random() * width ) + 1; 
     randy = Math.floor(Math.random() * height) + 1; 
-
+    
     console.log(imgpth)
-    console.log("x ==", x)
-    console.log("y ==", y)
-
+    console.log("x ==", randx)
+    console.log("y ==", randy)
+    
     mergeImages([
         {src: imgpth, width: 900, height: 700}, 
         {src: './resources/robHess.png', x: randx, y: randy},
     ])
-
+    
     .then(b64 => document.querySelector('.puzzle-photo').src = b64);
+    
+    
+}
 
+e2.onclick = e => {
+
+    console.log(e.offsetX)
+    console.log(e.offsetY)
+
+    if(e.offsetX >= randx - 80 && e.offsetX <= randx + 55){
+        if(e.offsetY >= randy - 65 && e.offsetY <= randy + 65) {
+            console.log("success!")
+        } 
+    }
+}
+
+
+var likeButton = document.querySelector('.like-icon')
+function handleLikeButtonClick() {
+    var likeIcon = document.querySelector('.like-icon i')
+    likeIcon.classList.remove('far')
+    likeIcon.classList.remove('fa-heart')
+    likeIcon.classList.add('fas')
+    likeIcon.classList.add('fa-heart')
+
+    var req = new XMLHttpRequest()
+    var reqUrl = '/gallery/addPuzzle'
+    console.log("== reqUrl:", reqUrl)
+    req.open('POST', reqUrl)
+
+    var puzzleName = document.querySelector('.puzzle-title strong').textContent
+    var puzzleLikes = parseInt(document.querySelector('.like.like-number').textContent) + 1
+
+    var puzzle = {
+        name: puzzleName,
+        path: c,
+        likes: puzzleLikes,
+        x: randx,
+        y: randy
+    }
+    var reqBody = JSON.stringify(puzzle)
+    console.log("== reqBody:", reqBody)
+    console.log("== typeof(reqBody):", typeof(reqBody))
+
+    req.setRequestHeader('Content-Type', 'application/json')
+
+    req.send(reqBody)
 
 }
+
+
+likeButton.addEventListener('click', handleLikeButtonClick)
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var express = require('express')
 var exphbs = require('express-handlebars')
+var fs = require('fs')
 
 var app = express()
 var port = process.env.PORT || 3000
@@ -8,13 +9,14 @@ var port = process.env.PORT || 3000
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 var puzzleData = require('./puzzleData.json')
+var rand
 
 
-
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/', function(req, res) { 
-    var rand = Math.floor(Math.random() * puzzleData.puzzles.length)
+    rand = Math.floor(Math.random() * puzzleData.puzzles.length)
     res.status(200).render('homePage', {
         name: puzzleData.puzzles[rand].name,
         path: puzzleData.puzzles[rand].path,
@@ -32,6 +34,37 @@ app.get('/gallery', function(req, res) {
 
 app.get('/about', function(req, res) {
     res.status(200).render('aboutPage')
+})
+
+app.post('/gallery/addPuzzle', function (req, res, next) {
+    console.log("== req.body:", req.body)
+    if (req.body) {
+        if (puzzleData.puzzles[rand]) {
+            puzzleData.gallery.push({
+                name: req.body.name,
+                path: req.body.path,
+                likes: req.body.likes,
+                x: req.body.x,
+                y: req.body.y
+            })
+            // console.log("== peopleData[" + person + "]:", peopleData[person])
+            fs.writeFile(
+                __dirname + '/puzzleData.json',
+                JSON.stringify(puzzleData, null, 2),
+                function (err) {
+                    if (err) {
+                        res.status(500).send("Error writing new data.  Try again later.")
+                    } else {
+                        res.status(200).send()
+                    }
+                }
+            )
+        } else {
+            next()
+        }
+    } else {
+        res.status(400).send("BRUH")
+    }
 })
 
 app.get('*', function(req, res) {
