58ea5a7cfe16f99a663de6f7c533cad821a8d05e
Dominick Tolomeo <tolomeodom@gmail.com>
2021-06-07 19:13:45 -0700

    activelink styling added

--- public/index.js
+++ public/index.js
@@ -1,3 +1,29 @@
+var currentURL = window.location.href
+var currRoute = currentURL.split('http://localhost:3000')[1]
+
+
+function makeLinkActive(route) {
+    var activeLink = document.querySelector(route)
+    activeLink.classList.add('active')
+}
+
+
+function findRoute() {
+    var route
+    if (currRoute === '/') {
+        route = "li.home a"
+    } 
+    else if (currRoute === '/gallery') {
+        route = "li.gallery a"
+    }
+    else if (currRoute === '/about') {
+        route = "li.about a"
+    }
+    makeLinkActive(route)
+}
+findRoute()
+
+
 var x,y;
 
 var width = document.querySelector('.puzzle-photo').width - 50
